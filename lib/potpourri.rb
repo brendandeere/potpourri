741e921d53f62162fa02462f937ed834ce333ca7
deereb <brendan.g.deere@gmail.com>
2015-11-10 20:41:39 -0800

    Require models

--- lib/potpourri.rb
+++ lib/potpourri.rb
@@ -1,4 +1,8 @@
 require "potpourri/version"
+require 'csv'
+
+ROOT = File.expand_path '../', __FILE__
+Dir.glob(File.join ROOT, 'potpourri/models/**/*').each { |f| require f }
 
 module Potpourri
   # Your code goes here...
