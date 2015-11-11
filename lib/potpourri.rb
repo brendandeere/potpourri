cd322553fb7e6cbc8c0056195ebdb55eb2160dcc
deereb <brendan.g.deere@gmail.com>
2015-11-11 12:48:42 -0800

    Also require lib

--- lib/potpourri.rb
+++ lib/potpourri.rb
@@ -3,6 +3,7 @@ require 'csv'
 
 ROOT = File.expand_path '../', __FILE__
 Dir.glob(File.join ROOT, 'potpourri/models/**/*').each { |f| require f }
+Dir.glob(File.join ROOT, 'potpourri/lib/**/*').each { |f| require f }
 
 module Potpourri
   # Your code goes here...
