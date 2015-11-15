9699461abf81f9e188cc83cb142f8292d98bdb55
deereb <brendan.g.deere@gmail.com>
2015-11-14 16:17:49 -0800

    Move Lib and model to be top level

--- lib/potpourri.rb
+++ lib/potpourri.rb
@@ -1,10 +1,8 @@
 require "potpourri/version"
-require 'csv'
 
 ROOT = File.expand_path '../', __FILE__
-Dir.glob(File.join ROOT, 'potpourri/models/**/*').each { |f| require f }
-Dir.glob(File.join ROOT, 'potpourri/lib/**/*').each { |f| require f }
 
 module Potpourri
-  # Your code goes here...
+  Dir.glob(File.join ROOT, 'lib/**/*').each { |f| require f }
+  Dir.glob(File.join ROOT, 'models/**/*').each { |f| require f }
 end
