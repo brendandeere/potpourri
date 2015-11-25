d85cc3b875d4da7eb4f3f49beb91ddf23618ba0c
deereb <brendan.g.deere@gmail.com>
2015-11-24 20:18:47 -0800

    Set up for active record specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,22 @@ require 'bundler'
 Bundler.require :default, :development
 
 require 'potpourri'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
+load File.dirname(__FILE__) + '/schema.rb'
+
+require File.dirname(__FILE__) + '/test_class.rb'
 
 RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
 
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
---
+++ spec/test_class.rb
@@ -0,0 +1,7 @@
+module Potpourri
+  module ActiveRecord
+    class TestClass < ::ActiveRecord::Base
+      self.table_name = 'test_class'
+    end
+  end
+end
