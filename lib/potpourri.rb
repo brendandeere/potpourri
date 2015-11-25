89628c7537dc36bc149d786af69e8a4b5a2d90ab
deereb <brendan.g.deere@gmail.com>
2015-11-24 20:16:57 -0800

    Move directories to the 'lib' level
    Saves on typing

--- lib/potpourri.rb
+++ lib/potpourri.rb
@@ -3,6 +3,7 @@ require "potpourri/version"
 ROOT = File.expand_path '../', __FILE__
 
 module Potpourri
-  Dir.glob(File.join ROOT, 'lib/**/*').each { |f| require f }
-  Dir.glob(File.join ROOT, 'models/**/*').each { |f| require f }
+  Dir.glob(File.join ROOT, 'lib/**/*.rb').each { |f| require f }
+  Dir.glob(File.join ROOT, 'models/**/*.rb').each { |f| require f }
+  Dir.glob(File.join ROOT, 'extensions/**/*.rb').each { |f| require f }
 end
